19ca7f4a16acb4657f2d58f049f181235209200e
AbnerAWR <AbnerW_@hotmail.com>
2020-11-17 00:37:13 -0300

    Objeto

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+//CODIGO NAO EXECUTAVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// Orientação objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+//Principios importante:
+//1.abstração
+//2.encapsulamento
+//3.herança
+//4.polimorfismo
+
