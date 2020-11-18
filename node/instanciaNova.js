e4254d4343952e43ae2368f3216de50f78c9feab
AbnerAWR <AbnerW_@hotmail.com>
2020-11-18 18:15:31 -0300

    node JS

---
+++ node/instanciaNova.js
@@ -0,0 +1,9 @@
+// Uma fatectory retorna um novo objeto
+module.exports = () => {
+    return {
+        valor: 1,
+        inc(){
+            this.valor++
+        }
+    }
+}
---
+++ node/this.js
@@ -0,0 +1,13 @@
+console.log(this === globalThis)
+console.log(this === module)
+console.log(this === module.exports)
+console.log(this === exports)
+
+function logThis() {
+    console.log('Dentro de uma funcao...')
+    console.log(this === exports)
+    console.log(this === module.exports)
+    console.log(this === global)
+}
+
+logThis()
