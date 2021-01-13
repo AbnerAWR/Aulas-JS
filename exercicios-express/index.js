073e6c9dd8be3f83b06d5bf7f1f0b01b6ac674b0
AbnerAWR <AbnerW_@hotmail.com>
2021-01-12 23:35:59 -0300

    Express

---
+++ exercicios-express/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+
+app.use('/opa', (req, res, next) => {
+    console.log('Antes....')
+    next()
+})
+
+app.get('/opa', (req, res) => {
+    console.log('Durante....')
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1},
+            {id: 34, name: 'Bia', position: 2},
+            {id: 73, name: 'Carlos', position: 3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+    
+        // res.send('Estou bem!')
+    
+        // res.json ({
+        //     name: "Ipad 32Gb",
+        //     price: 1899.00,
+        //     discount: 0.12
+        // })
+    })
+    
+app.use((req, res, next) => {
+    console.log('Depois....')
+    next()
+})
+
+app.listen(3000, () => {
+    console.log('Backend executando.....')
+})
